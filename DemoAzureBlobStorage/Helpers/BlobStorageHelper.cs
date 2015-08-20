2b1e1c07dff2d82c195589ebbfe104b6918fcfbf
José Saturnino <isc_saturnino@outlook.com>
2015-08-20 03:15:36 -0500

    First commit

---
+++ DemoAzureBlobStorage/Helpers/BlobStorageHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace DemoAzureBlobStorage.Helpers
+{
+    public class BlobStorageHelper
+    {
+
+        private readonly CloudStorageAccount _storageAccount;
+
+        private string _accountName = "saturblobtest";
+        string _key = "cVIkkZ51Je/qRRtlJzk6GBTRv058pdh9z78h9+JRN+6aRZNIAXgbj/jOuOn7tBFV0kY/oIVTOgvKfZrRg/lO3w==";
+
+        public BlobStorageHelper()
+        {
+            StorageCredentials storageCredentials = new StorageCredentials(_accountName, _key);
+            _storageAccount = new CloudStorageAccount(storageCredentials, true);
+        }
+
+        public async Task UploadImageAsync(string containerName, string fileName, StorageFile file)
+        {
+            CloudBlobClient cloudBlobClient = _storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(containerName);
+
+            await cloudBlobContainer.CreateIfNotExistsAsync();
+
+            CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(fileName);
+
+
+            await cloudBlockBlob.UploadFromFileAsync(file);
+        }
+
+    }
+}
---
+++ DemoAzureBlobStorage/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using DemoAzureBlobStorage.Helpers;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DemoAzureBlobStorage
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private StorageFile _storageFile;
+
+        private async void btnTakeImage_OnClick(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI cameraCaptureUi = new CameraCaptureUI();
+
+            cameraCaptureUi.PhotoSettings.AllowCropping = false;
+            cameraCaptureUi.PhotoSettings.MaxResolution = CameraCaptureUIMaxPhotoResolution.MediumXga;
+
+            _storageFile =
+               await cameraCaptureUi.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            if (_storageFile != null)
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.SetSource(await _storageFile.OpenReadAsync());
+                CurrentImage.Source = bitmapImage;
+            }
+
+        }
+
+        private async void btnUploadImage_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (_storageFile != null)
+            {
+                if (!string.IsNullOrEmpty(txtName.Text) && !string.IsNullOrEmpty(txtContainerName.Text))
+                {
+                    BlobStorageHelper blobStorageHelper = new BlobStorageHelper();
+
+                    await blobStorageHelper.UploadImageAsync(txtContainerName.Text, $"{txtName.Text}.jpg", _storageFile);
+
+                    MessageDialog messageDialog = new MessageDialog("La imagen se ha subido con éxito.");
+                    await messageDialog.ShowAsync();
+                }
+                else
+                {
+                    MessageDialog messageDialog = new MessageDialog("Necesitas asignar un nombre a la imagen y al contenedor");
+                    await messageDialog.ShowAsync();
+                }
+            }
+            else
+            {
+                MessageDialog messageDialog = new MessageDialog("Debes tener una imagen para subir al blob");
+                await messageDialog.ShowAsync();
+            }
+
+        }
+    }
+}
